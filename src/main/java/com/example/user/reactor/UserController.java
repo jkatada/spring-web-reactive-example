d3fdca57f19360109a88643a4fe30d283a1f6033
jkatada <jkatada>
2016-11-05 17:57:11 +0900

    Create reactor package in user package

---
+++ src/main/java/com/example/user/reactor/UserController.java
@@ -0,0 +1,24 @@
+package com.example.user.reactor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.user.User;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@GetMapping("/listAdult")
+	public Flux<User> listAdult() {
+		// 20歳以上のユーザを返す
+		return userRepository.findAll()
+				.filter(u -> u.getAge() >= 20);
+	}
+
+}
