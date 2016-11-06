806ab09d37f8f9ad35cd1428f43a9091ccac24be
jkatada <jkatada>
2016-11-07 00:51:08 +0900

    Change TopicProcessor to use share method

--- src/main/java/com/example/chat/MessageSender.java
+++ src/main/java/com/example/chat/MessageSender.java
@@ -2,16 +2,14 @@ package com.example.chat;
 
 import org.springframework.stereotype.Component;
 
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.TopicProcessor;
 
 @Component
-@Slf4j
 public class MessageSender {
 
-	private TopicProcessor<Message> topic = TopicProcessor.create();
+	private TopicProcessor<Message> topic = TopicProcessor.share(true);
 
 	public Flux<Message> connect() {
 		return Flux.from(topic);
