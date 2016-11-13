c4014c5254797c33e01c5460ffbcf90416e94e45
jkatada <jkatada>
2016-11-13 13:09:22 +0900

    Refine MongoRepository

--- src/main/java/com/example/mongo/MongoRepository.java
+++ src/main/java/com/example/mongo/MongoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import com.example.model.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.reactivestreams.client.MongoCollection;
 import com.mongodb.reactivestreams.client.MongoDatabase;
 
 import reactor.core.publisher.Flux;
@@ -15,27 +16,26 @@ import reactor.core.publisher.Mono;
 @Repository
 public class MongoRepository {
 
-	@Autowired
-	MongoDatabase mongodb;
+	private final ObjectMapper mapper;
+	private final MongoCollection<Document> col;
 
 	@Autowired
-	ObjectMapper mapper;
+	public MongoRepository(MongoDatabase db, ObjectMapper mapper) {
+		this.mapper = mapper;
+		this.col = db.getCollection("user");
+	}
 
 	public Mono<Void> insert(User user) {
-		return Mono.empty(
-				mongodb.getCollection("user").insertOne(Document.parse(toJson(user))));
+		return Mono.empty(col.insertOne(Document.parse(toJson(user))));
 	}
 
 	public Mono<Void> insert(Mono<User> user) {
-		return user.flatMap(
-				u -> mongodb.getCollection("user").insertOne(Document.parse(toJson(u))))
-				.then();
+		return user.flatMap(u -> col.insertOne(Document.parse(toJson(u)))).then();
 	}
 
 	public Flux<User> findAll() {
-		return Flux.from(mongodb.getCollection("user").find())
-				.map(d -> new User(d.getInteger("id"), d.getString("name"),
-						d.getInteger("age")));
+		return Flux.from(col.find()).map(d -> new User(d.getInteger("id"),
+				d.getString("name"), d.getInteger("age")));
 	}
 
 	private String toJson(User user) {
